d6aeb851abfc12d5219dd1231fc60497db1044f8
Kadriye Nur <65511863+kadriyenurnlbnt@users.noreply.github.com>
2020-12-04 16:20:49 +0300

    Metod Çağırma

---
+++ Metod Çağırma/uyg9/anasayfa.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Runtime.CompilerServices;
+
+namespace uyg9
+{
+    public partial class anasayfa : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            isimolustur("kadriye nur" , "Nalbant");
+        }
+        protected void isimolustur(string ad, string soyad,
+            [CallerFilePath] string dosya = "",
+            [CallerMemberName] string metod = "",
+            [CallerLineNumber] int satir = -1)
+        {
+            string isim = ad + " " + soyad;
+            Label1.Text = "Kişinin ismi: " + isim + "<br/>" + " <b>Metodu çağıran metodun bilgileri </b><br/>" +
+                "- Dosya: " + dosya + "<br/>" +
+                "- Metod: " + metod + "<br/>" +
+                "- Satır: " + satir;
+        }
+
+    }
+}

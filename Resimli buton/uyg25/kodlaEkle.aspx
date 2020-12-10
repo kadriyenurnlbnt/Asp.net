1b07a3a12582e861747b0b1ea095c38654b9159b
Kadriye Nur <65511863+kadriyenurnlbnt@users.noreply.github.com>
2020-12-10 17:49:00 +0300

    Resimli buton

---
+++ Resimli buton/uyg25/kodlaEkle.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="kodlaEkle.aspx.cs" Inherits="uyg25.kodlaEkle" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:ImageButton ID="ImageButton1" runat="server" OnClick="ImageButton1_Click" />
+&nbsp;</div>
+    </form>
+</body>
+</html>
---
+++ Resimli buton/uyg25/kodlaEkle.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace uyg25
+{
+    public partial class kodlaEkle : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ImageButton1.ImageUrl = "Yazilim.jpg";
+        }
+
+        protected void ImageButton1_Click(object sender, ImageClickEventArgs e)
+        {
+            Response.Redirect("https://nurnlbnt.com");
+        }
+    }
+}

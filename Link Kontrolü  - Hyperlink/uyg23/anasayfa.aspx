723ea5f8cd21b52ded42767ae626893a4a9d798d
Kadriye Nur <65511863+kadriyenurnlbnt@users.noreply.github.com>
2020-12-10 22:14:41 +0300

    Link Kontrolü  - Hyperlink

---
+++ Link Kontrolü  - Hyperlink/uyg23/anasayfa.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="anasayfa.aspx.cs" Inherits="uyg23.anasayfa" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:HyperLink ID="HyperLink1" runat="server">HyperLink</asp:HyperLink>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Link Kontrolü  - Hyperlink/uyg23/anasayfa.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace uyg23
+{
+    public partial class anasayfa : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HyperLink1.NavigateUrl = "https://nurnlbnt.com";
+            HyperLink1.Text = "Kadriye Nur";
+            HyperLink1.ForeColor = System.Drawing.Color.Red;
+        }
+    }
+}

7495008f6f69b1fa896c44716ab69385ac45e165
Kadriye Nur <65511863+kadriyenurnlbnt@users.noreply.github.com>
2020-12-21 16:04:24 +0300

    Substitution kullanımı

---
+++ Substitution kullanımı/uyg36/anasayfa2.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace uyg36
+{
+    public partial class anasayfa2 : System.Web.UI.Page
+    {
+        public static string KayitGetir (HttpContext context)
+        {
+            return DateTime.Now.ToString();
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
